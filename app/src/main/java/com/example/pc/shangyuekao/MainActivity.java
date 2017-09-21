03bd75487cfbbfb086544757b4e3d88373f6c1db
chengzhuoyan <2489432491@qq.com>
2017-09-21 09:30:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pc/shangyuekao/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.pc.shangyuekao;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.example.pc.shangyuekao.adapter.MyFragmentPagerAdapter;
+
+/**
+ * 1)	Fragment+ViewPager实现底部导航栏对应的三个Fragment滑动切换
+ * A．三个Fragment定义正确5分）
+ * B．FragmentPagerAdapter定义正确（5分）
+ * C．三个Fragment通过ViewPager可以实现滑动切换(5分)
+ */
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener, ViewPager.OnPageChangeListener {
+
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+    private TextView txt_topbar;
+    private RadioGroup rg_tab_bar;
+    private RadioButton rb_channel;
+    private RadioButton rb_message;
+    private RadioButton rb_better;
+    private ViewPager vpager;
+
+    private MyFragmentPagerAdapter mAdapter;
+
+    //几个代表页面的常量
+    public static final int PAGE_ONE = 0;
+    public static final int PAGE_TWO = 1;
+    public static final int PAGE_THREE = 2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        bindViews();
+        rb_channel.setChecked(true);
+    }
+
+    private void bindViews() {
+        txt_topbar = (TextView) findViewById(R.id.txt_topbar);
+        rg_tab_bar = (RadioGroup) findViewById(R.id.rg_tab_bar);
+        rb_channel = (RadioButton) findViewById(R.id.rb_channel);
+        rb_message = (RadioButton) findViewById(R.id.rb_message);
+        rb_better = (RadioButton) findViewById(R.id.rb_better);
+        rg_tab_bar.setOnCheckedChangeListener(this);
+
+        vpager = (ViewPager) findViewById(R.id.vpager);
+        vpager.setAdapter(mAdapter);
+        vpager.setCurrentItem(0);
+        vpager.addOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.rb_channel:
+                vpager.setCurrentItem(PAGE_ONE);
+                break;
+            case R.id.rb_message:
+                vpager.setCurrentItem(PAGE_TWO);
+                break;
+            case R.id.rb_better:
+                vpager.setCurrentItem(PAGE_THREE);
+                break;
+        }
+    }
+
+
+    //重写ViewPager页面切换的处理方法
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+        //state的状态有三个，0表示什么都没做，1正在滑动，2滑动完毕
+        if (state == 2) {
+            switch (vpager.getCurrentItem()) {
+                case PAGE_ONE:
+                    rb_channel.setChecked(true);
+                    txt_topbar.setText("圈子");
+                    break;
+                case PAGE_TWO:
+                    rb_message.setChecked(true);
+                    txt_topbar.setText("朋友");
+                    break;
+                case PAGE_THREE:
+                    rb_better.setChecked(true);
+                    txt_topbar.setText("我的");
+                    break;
+            }
+        }
+    }
+}
